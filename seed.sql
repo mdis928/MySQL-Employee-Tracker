8266f1f4eb147233e67dd7bf2d2c041ec2f1fb90
Michael DiSanto <mdisanto928@gmail.com>
2021-05-17 09:53:11 -0400

    table names created

--- seed.sql
+++ seed.sql
@@ -1,1 +1,50 @@
-seed.sql
+DROP DATABASE IF EXISTS EmployeeDB;
+CREATE database EmployeeDB;
+
+USE EmployeeDB;
+
+CREATE TABLE department (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  song VARCHAR(100) NULL,
+  year INT NULL,
+  raw_total DECIMAL(10,4) NULL,
+  raw_usa DECIMAL(10,4) NULL,
+  raw_uk DECIMAL(10,4) NULL,
+  raw_eur DECIMAL(10,4) NULL,
+  raw_row DECIMAL(10,4) NULL,
+  PRIMARY KEY (position)
+);
+
+CREATE TABLE roles (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  album VARCHAR(100) NULL,
+  year INT NULL,
+  raw_total DECIMAL(10,4) NULL,
+  raw_usa DECIMAL(10,4) NULL,
+  raw_uk DECIMAL(10,4) NULL,
+  raw_eur DECIMAL(10,4) NULL,
+  raw_row DECIMAL(10,4) NULL,
+  PRIMARY KEY (position)
+);
+
+CREATE TABLE employee (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  album VARCHAR(100) NULL,
+  year INT NULL,
+  raw_total DECIMAL(10,4) NULL,
+  raw_usa DECIMAL(10,4) NULL,
+  raw_uk DECIMAL(10,4) NULL,
+  raw_eur DECIMAL(10,4) NULL,
+  raw_row DECIMAL(10,4) NULL,
+  PRIMARY KEY (position)
+);
+
+
+
+
+
+SELECT * FROM top5000;
+select * from top_albums;
