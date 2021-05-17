87f86ad6428aa6e3c92dc3ed49aaf490bc4ddae7
Michael DiSanto <mdisanto928@gmail.com>
2021-05-17 11:20:32 -0400

    added code to index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-index.js
+// For MySQL database
+const mysql = require('mysql'),
+// For NPM inquirer so that we can run prompts
+const inquirer = require("inquirer"),
+// These two requires will work together so that we can create an interactive employee DB
+
+const connection = mysql.createConnection({
+    // use local host
+    host: 'localhost',
+
+    // Use port 3306
+    port: 3306,
+
+    // Will be using root user
+    user: 'root',
+
+    // When testing make sure to use your password. Make sure to delete password when you do a git push or pull. Talk to TA and instructor about this
+    password: '',
+    database: 'Employee_DB',
+
+})
